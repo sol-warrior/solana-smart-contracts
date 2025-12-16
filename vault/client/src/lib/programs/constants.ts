8afeaa75adbe90d90c7287fc20b2de757aa55717
solwarrior <118261237+sol-warrior@users.noreply.github.com>
2025-12-17 04:03:23 +0530

    feat: add constants for Vault program and user vault seeds
    - Introduced a new `constants.ts` file to define the Vault program ID and user vault seed constants.
    - Included constants for user vault lamports seed to facilitate wallet interactions within the application.

---
+++ vault/client/src/lib/programs/constants.ts
@@ -0,0 +1,7 @@
+import { address } from '@solana/kit';
+
+export const VAULT_PROGRAM_ID = address('DJktBCt1vV8jdYmyxqnH8oNVa5PMLWgkc7WuT4KB1Q8o');
+
+
+export const USER_VAULT_SEED = [118, 97, 117, 108, 116];
+export const USER_VAULT_LAMPORTS_SEED = [117, 115, 101, 114, 95, 108, 97, 109, 112, 111, 114, 116, 115];
