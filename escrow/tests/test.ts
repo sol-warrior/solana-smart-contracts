de46ab3af8c5e1c05d77b55e1daaba00520959b2
solwarrior <118261237+sol-warrior@users.noreply.github.com>
2025-12-23 19:36:24 +0530

    fix(tests): correct test suite name and update import type in escrow tests
    - Fixed the test suite name from "escrwo" to "escrow" in `test.ts` for accuracy.
    - Updated the import statement for the `Escrow` type to use the correct file extension.
    - Commented out the initialization test for future implementation, maintaining the test structure.

--- escrow/tests/test.ts
+++ escrow/tests/test.ts
@@ -1,16 +1,16 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { Escrow } from "../target/types/escrow";
+import type { Escrow } from "../target/types/escrow.ts";
 
-describe("escrwo", () => {
+describe("escrow", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.escrwo as Program<Escrow>;
+  const program = anchor.workspace.escrow as Program<Escrow>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+  // it("Is initialized!", async () => {
+  //   // Add your test here.
+  //   const tx = await program.methods.initialize().rpc();
+  //   console.log("Your transaction signature", tx);
+  // });
 });
